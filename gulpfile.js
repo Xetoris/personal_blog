1817b4dbabe9f585a7557f9e320a7d25748f2d27
xetoris <cmccord4pa@gmail.com>
2016-02-03 02:11:14 -0500

    Changes: 1)  Playing around with Gulp. Got it sass'ing bootstrap!

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
-    // place code for your default task here
+var config = {
+    bootstrapDir: './apps/web/assets/bower/bootstrap',
+    publicDir: './public'
+};
+
+gulp.task('css', function() {
+    return gulp.src(config.bootstrapDir + '/scss/bootstrap-flex.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(config.publicDir + '/css'));
 });
+
+gulp.task('lint', function(){
+    // When I write some javascript, Lint it!
+});
+
+gulp.task('default', ['lint', 'css']);
