6bf6b2d7eaf2ac80e4df95c7d3688250a4fdfa34
xetoris <cmccord4pa@gmail.com>
2016-02-04 02:33:59 -0500

    Changes: 1)  So Hanami wants to serve its assets and manages doing so by putting them in the public/assets/  folder. I adjusted bower to install in just ./bower_components, and Gulp now dumps the files into ./apps/web/assets.  There, Hanami will copy them to public once they have been requested.
    2) Lots of changes based on that.

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,29 @@ var sass = require('gulp-sass');
 var eslint = require('gulp-eslint');
 
 var config = {
-    bootstrapDir: './apps/web/assets/bower/bootstrap',
-    publicDir: './public'
+    bootstrapDir: './bower_components/bootstrap',
+    jqueryDir: './bower_components/jquery',
+    tetherDir: './bower_components/tether',
+    assetDir: './apps/web/assets'
 };
 
 gulp.task('css', function() {
     return gulp.src(config.bootstrapDir + '/scss/bootstrap-flex.scss')
         .pipe(sass())
-        .pipe(gulp.dest(config.publicDir + '/css'));
+        .pipe(gulp.dest(config.assetDir + '/css'));
 });
 
 gulp.task('lint', function(){
     // When I write some javascript, Lint it!
 });
 
-gulp.task('default', ['lint', 'css']);
+gulp.task('js', function(){
+   return gulp.src(config.bootstrapDir + '/dist/js/bootstrap.min.js')
+       .pipe(gulp.dest(config.assetDir + '/js'))
+    && gulp.src(config.jqueryDir + '/dist/jquery.min.js')
+           .pipe(gulp.dest(config.assetDir+'/js'))
+    && gulp.src(config.tetherDir + '/dist/js/tether.min.js')
+           .pipe(gulp.dest(config.assetDir+'/js'));
+});
+
+gulp.task('default', ['lint', 'css', 'js']);
