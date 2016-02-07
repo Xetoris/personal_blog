2edf284784cfa9e504364d409027ea5abf4251cf
xetoris <cmccord4pa@gmail.com>
2016-02-07 04:31:57 -0500

    Changes: 1)  Added some... what I suspect... are temporary files while I figure out the hanami + gulp situation. 2) Adjusted home page for tri-image navbar. 3) Other stuffs!

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,17 @@ var config = {
     bootstrapDir: './bower_components/bootstrap',
     jqueryDir: './bower_components/jquery',
     tetherDir: './bower_components/tether',
-    assetDir: './apps/web/assets'
+    assetDir: './apps/web/assets',
+    publicDir: './apps/web/assets/public'
 };
 
 gulp.task('css', function() {
     return gulp.src(config.bootstrapDir + '/scss/bootstrap-flex.scss')
         .pipe(sass())
-        .pipe(gulp.dest(config.assetDir + '/css'));
+        .pipe(gulp.dest(config.publicDir + '/css'))
+        && gulp.src(config.assetDir + '/sass/personal_blog.css')
+            .pipe(sass())
+            .pipe(gulp.dest(config.publicDir +'/css'));
 });
 
 gulp.task('lint', function(){
@@ -21,11 +25,16 @@ gulp.task('lint', function(){
 
 gulp.task('js', function(){
    return gulp.src(config.bootstrapDir + '/dist/js/bootstrap.min.js')
-       .pipe(gulp.dest(config.assetDir + '/js'))
+       .pipe(gulp.dest(config.publicDir + '/js'))
     && gulp.src(config.jqueryDir + '/dist/jquery.min.js')
-           .pipe(gulp.dest(config.assetDir+'/js'))
+           .pipe(gulp.dest(config.publicDir+'/js'))
     && gulp.src(config.tetherDir + '/dist/js/tether.min.js')
-           .pipe(gulp.dest(config.assetDir+'/js'));
+           .pipe(gulp.dest(config.publicDir+'/js'));
 });
 
-gulp.task('default', ['lint', 'css', 'js']);
+gulp.task('imgs', function(){
+    return gulp.src(config.assetDir + '/images/*.{png,jpg}')
+        .pipe(gulp.dest(config.publicDir + '/imgs'));
+});
+
+gulp.task('default', ['lint', 'css', 'js', 'imgs']);
