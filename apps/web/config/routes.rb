e03a5fd2bc66603d553dbf88700ed7254879f896
xetoris <cmccord4pa@gmail.com>
2016-02-10 21:21:44 -0500

    Placeholder routes.

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -2,3 +2,6 @@
 # See: http://www.rubydoc.info/gems/hanami-router/#Usage
 
 get '/', to: 'home#index'
+#get '/gaming', to: 'gaming#list'
+#get '/coding', to: 'coding#list'
+#get '/tabletop', to: 'tabletop#list'
